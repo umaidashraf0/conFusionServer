c3912334327ab95ed8508de659693e3b1a2fffb0
Maham Nazir <maham_nazir19@yahoo.com>
2019-05-05 14:55:08 +0500

    File Uploading

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
@@ -49,6 +50,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/imageUpload',uploadRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
